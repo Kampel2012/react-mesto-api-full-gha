fb4ace09ec64bed7863bde7d8665ac49cff500f8
Anton Glazunov <kampel2012@yandex.ru>
2023-07-12 00:32:27 +0500

    fix jwt palevo

--- backend/utils/jwt.js
+++ backend/utils/jwt.js
@@ -1,6 +1,11 @@
 import jwt from 'jsonwebtoken';
 
-const SECRET_KEY = 'e041e9c9fbc63d5ba0de72298f8d8f54';
+const SECRET_KEY = '';
+const SECRET_KEY_DEV = 'eyJhbGciOiJIUzI1NiJ9';
+
+if (SECRET_KEY === SECRET_KEY_DEV) {
+  console.warn('Надо исправить. В продакшне используется тот же секретный ключ, что и в режиме разработки.');
+}
 
 export function generateToken(_id) {
   return jwt.sign({ _id }, SECRET_KEY, {
